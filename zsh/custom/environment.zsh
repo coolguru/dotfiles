c703f4d08d1bf874128082bbcdb1b5f4a70dca50
Jacob Reynolds <jacob.e.reynolds@gmail.com>
2017-01-18 09:58:57 -0800

    Add Go binary to path

--- zsh/custom/environment.zsh
+++ zsh/custom/environment.zsh
@@ -11,11 +11,15 @@ then
   source ${COREO_CONF}
 fi
 
-
-export PATH="$HOME/.rvm/bin:$PATH" # Add RVM to PATH for scripting
+# Load RVM scripts and path
+export PATH="$HOME/.rvm/bin:$PATH"
 source ~/.rvm/scripts/rvm
 
+# Load NVM scripts and path
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+# Load Go path
+export PATH=$PATH:/usr/local/go/bin
 
 export PATH=$PATH:~/.local/bin
